7d17119f04ab15c317f9a0ca39d6e972f1db5041
hackerton <9619080+HackerTon@users.noreply.github.com>
2023-10-27 06:35:24 +0800

    Add gpu support

--- benchmark.py
+++ benchmark.py
@@ -6,17 +6,24 @@ from torchvision.models.detection import (
 )
 from torchvision.utils import draw_bounding_boxes
 
+if torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu'
+
 # Step 1: Initialize model with the best available weights
 weights = FasterRCNN_MobileNet_V3_Large_320_FPN_Weights.DEFAULT
 model = fasterrcnn_mobilenet_v3_large_320_fpn(weights=weights, box_score_thresh=0.9)
 model.eval()
+model = model.to(device)
 
 # Step 2: Initialize the inference transforms
 preprocess = weights.transforms()
+preprocess = preprocess.to(device)
 
 while True:
     initial_time = time.time()
-    img = torch.rand([3, 1080, 1920]).to(torch.uint8)
+    img = torch.rand([3, 1080, 1920]).to(torch.uint8).to(device)
     batch = preprocess(img).unsqueeze(0)
 
     with torch.no_grad():
